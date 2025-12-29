07d1c833430db0f0e06e9b11283e7d5042ca0cc1
smrlyman <smrlyman@gmail.com>
2025-12-29 14:56:45 +0200

    feat: display splitted text and current symbol

--- src/components/common/TextType.tsx
+++ src/components/common/TextType.tsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 const startText =
   "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, iure inventore repudiandae officiis delectus illo magnam dolores. Eius architecto, similique enim quam aut dignissimos nihil ullam hic corporis incidunt aliquid";
 
+const DOT_WITH_ZERO_SPACE = "\u00B7\u200B";
+
 const TextType = () => {
   const [typedText, setTypedText] = useState<string>("");
   const [textLeft, setTextLeft] = useState(startText);
@@ -25,11 +27,14 @@ const TextType = () => {
   }, []);
 
   return (
-    <p className="text-4xl/14">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, iure
-      inventore repudiandae officiis delectus illo magnam dolores. Eius
-      architecto, similique enim quam aut dignissimos nihil ullam hic corporis
-      incidunt aliquid.
+    <p className="text-4xl/14 w-max font-mono">
+      <span className="text-gray-500">
+        {typedText.replaceAll(" ", DOT_WITH_ZERO_SPACE)}
+      </span>
+      <span className="text-green-600">
+        {textLeft.charAt(0).replaceAll(" ", DOT_WITH_ZERO_SPACE)}
+      </span>
+      <span>{textLeft.slice(1).replaceAll(" ", DOT_WITH_ZERO_SPACE)}</span>
     </p>
   );
 };
