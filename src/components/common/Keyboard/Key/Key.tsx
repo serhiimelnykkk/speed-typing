4f97aa56a4f8bafea5fd285853a3c61b94ba87d2
smrlyman <smrlyman@gmail.com>
2025-12-30 20:45:38 +0200

    feat: make keys font size responsive

--- src/components/common/Keyboard/Key/Key.tsx
+++ src/components/common/Keyboard/Key/Key.tsx
@@ -9,7 +9,7 @@ interface KeyProps {
 const Key = memo(({ isPressed, keyboardKey }: KeyProps) => {
   return (
     <div
-      className={`relative h-12 bg-gray-300 border border-gray-900
+      className={`relative h-12 bg-gray-300 border border-gray-900 text-xs sm:text-base
                 ${isPressed ? "bg-green-700 text-gray-50" : ""} 
               rounded-sm font-bold w-12 ${keyboardKey.styles}`}
       key={keyboardKey.mainSymbol}
@@ -17,9 +17,7 @@ const Key = memo(({ isPressed, keyboardKey }: KeyProps) => {
       <div className="absolute top-1 left-1">
         {keyboardKey.visualName || keyboardKey.mainSymbol}
       </div>
-      <div className="absolute bottom-1 right-1 text-sm">
-        {keyboardKey.shiftSymbol}
-      </div>
+      <div className="absolute bottom-1 right-1">{keyboardKey.shiftSymbol}</div>
     </div>
   );
 });
