53103e3d205a4147841425c3ede45990ddc50155
smrlyman <smrlyman@gmail.com>
2026-01-02 13:36:45 +0200

    feat: listen to pressing Escape to pause/unpause

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,13 +1,31 @@
 import TextType from "../../common/TextType/TextType";
 import Keyboard from "../../common/Keyboard/Keyboard";
+import { PauseContext } from "../../../context/PauseContext";
+import { useEffect, useState } from "react";
 
 const Main = () => {
+  const [isPaused, setIsPaused] = useState(false);
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setIsPaused((prev) => !prev);
+      }
+    };
+
+    addEventListener("keydown", onKeyDown);
+
+    return () => removeEventListener("keydown", onKeyDown);
+  }, []);
+
   return (
     <main className="h-full px-10">
-      <div className="flex h-[50%] items-end justify-center">
-        <TextType />
-      </div>
-      <Keyboard />
+      <PauseContext value={isPaused}>
+        <div className="flex h-[50%] items-end justify-center">
+          <TextType />
+        </div>
+        <Keyboard />
+      </PauseContext>
     </main>
   );
 };
