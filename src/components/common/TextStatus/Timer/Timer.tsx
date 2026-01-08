ed56c9fecddd4a22c8f80e6aaf87cbe145266835
smrlyman <smrlyman@gmail.com>
2026-01-08 11:54:44 +0200

    feat: allow user to input duration

--- src/components/common/TextStatus/Timer/Timer.tsx
+++ src/components/common/TextStatus/Timer/Timer.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
-const duration = 30_000;
-
 const Timer = () => {
   const [timeRemaining, setTimeRemaining] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  console.log("\nrerender\n");
 
   const startTimer = () => {
-    if (timeRemaining > 0) return;
+    if (timeRemaining > 0 || duration <= 0) return;
 
     const endTime = duration + performance.now();
+
     const interval = setInterval(() => {
       const remaining = Math.ceil((endTime - performance.now()) / 1000);
       if (remaining <= 0) {
@@ -20,12 +22,26 @@ const Timer = () => {
     }, 100);
   };
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const duration = parseInt(event.target.value);
+    setDuration(duration);
+  };
+
   return (
     <div className="flex gap-4">
       <span>Timer: {timeRemaining}</span>
       <button onClick={startTimer} className="cursor-pointer">
         Start
       </button>
+      <label>
+        <span>Time in Milleseconds</span>
+      </label>
+      <input
+        type="text"
+        value={duration}
+        onChange={handleChange}
+        className="border border-gray-500"
+      />
     </div>
   );
 };
