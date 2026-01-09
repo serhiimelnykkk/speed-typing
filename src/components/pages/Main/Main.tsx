908cc9fd44c28ee328db2c8f5b1adadfec73be8a
smrlyman <smrlyman@gmail.com>
2026-01-09 18:31:27 +0200

    refactor: create Typing component

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,17 +1,12 @@
-import TypingArea from "../../common/TypingArea/TypingArea";
-import Keyboard from "../../common/Keyboard/Keyboard";
-import TextStatus from "../../common/TextStatus/TextStatus";
 import { PauseContext } from "../../../context/PauseContext";
-import { useEffect, useState } from "react";
-import {
-  WpmStateContext,
-  WpmDispatchContext,
-} from "../../../context/WpmContext";
 import { PauseLockContext } from "../../../context/PauseLockContext";
+import { useEffect, useState } from "react";
+
+import Typing from "./Typing/Typing";
 
 const Main = () => {
   const [isPaused, setIsPaused] = useState(false);
-  const [wordsPerMinute, setWordsPerMinute] = useState(0);
+
   const [isPauseLocked, setIsPauseLocked] = useState(false);
 
   useEffect(() => {
@@ -30,15 +25,7 @@ const Main = () => {
     <PauseContext value={isPaused}>
       <PauseLockContext value={setIsPauseLocked}>
         <main className="px-10 h-full max-w-350 mx-auto">
-          <div className="flex flex-col h-[50%] justify-end">
-            <WpmStateContext value={wordsPerMinute}>
-              <TextStatus />
-            </WpmStateContext>
-            <WpmDispatchContext value={setWordsPerMinute}>
-              <TypingArea />
-            </WpmDispatchContext>
-          </div>
-          <Keyboard />
+          <Typing />
         </main>
       </PauseLockContext>
     </PauseContext>
