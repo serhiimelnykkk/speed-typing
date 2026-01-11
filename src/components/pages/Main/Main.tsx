5b4c7225a05e493a2a654a7066d5e8260ee71e08
smrlyman <smrlyman@gmail.com>
2026-01-11 12:17:14 +0200

    refactor: get active component by passing current view type to object instead of ternary operators chain

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,14 +1,14 @@
-import Stats from "@/components/pages/Main/Stats/Stats";
-import Typing from "@/components/pages/Main/Typing/Typing";
-
 import { useMainViewContext } from "@/context/MainViewContext/Context";
+import { MainViews } from "@/components/pages/Main/MainViews";
 
 const Main = () => {
   const { mainView } = useMainViewContext();
 
+  const ActiveView = MainViews[mainView];
+
   return (
     <main className="px-10 h-full max-w-350 mx-auto">
-      {mainView === "typing" ? <Typing /> : mainView === "stats" && <Stats />}
+      <ActiveView />
     </main>
   );
 };
