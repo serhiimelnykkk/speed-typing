4c56960590b5e5994d217f049342178c01c92f7b
smrlyman <smrlyman@gmail.com>
2026-01-14 19:00:06 +0200

    feat: save wpm in localStorage

--- src/context/WpmContext/Provider.tsx
+++ src/context/WpmContext/Provider.tsx
@@ -1,12 +1,19 @@
 import { WpmContext } from "@/context/WpmContext/Context";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 interface Props {
   children?: React.ReactNode;
 }
 
+const storedWpm = JSON.parse(localStorage.getItem("wpm") || "{}")["wpm"];
+const initialWpm = storedWpm ? storedWpm : 0;
+
 export const WpmContextProvider = ({ children }: Props) => {
-  const [wpm, setWpm] = useState(0);
+  const [wpm, setWpm] = useState(initialWpm);
+
+  useEffect(() => {
+    localStorage.setItem("wpm", JSON.stringify({ wpm }));
+  }, [wpm]);
 
   const contextValue = useMemo(() => {
     return { wpm, setWpm };
