2fd554d67a1d0631f160dc9301f54ac6c1456510
smrlyman <smrlyman@gmail.com>
2026-01-18 00:50:19 +0200

    feat: update PauseStatus styles

--- src/components/common/TextStatus/PauseStatus/PauseStatus.tsx
+++ src/components/common/TextStatus/PauseStatus/PauseStatus.tsx
@@ -4,21 +4,17 @@ const PauseOverlay = () => {
   const { isPaused } = usePauseContext();
 
   return (
-    <>
-      <div>
-        Pause{" "}
-        <kbd className="bg-gray-600 px-2 py-1 text-sm text-gray-50 rounded-sm font-mono align-middle">
-          Esc
-        </kbd>{" "}
-        <span
-          className={`inline-block ${
-            isPaused ? "bg-green-600" : "bg-red-600"
-          } px-2 py-1 w-12 text-center font-bold text-gray-50 rounded-sm`}
-        >
-          {isPaused ? "ON" : "OFF"}
-        </span>
-      </div>
-    </>
+    <div>
+      Pause{" "}
+      <kbd
+        className={`inline-block ${
+          isPaused ? "bg-green-600" : "bg-red-600"
+        } px-2 py-1 w-12 text-center font-bold text-gray-50 rounded-sm
+          transition-colors duration-200`}
+      >
+        Esc
+      </kbd>
+    </div>
   );
 };
 
