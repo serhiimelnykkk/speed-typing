5bc38a6d0f9a9b8cf6027ec43b91ff0a9a0bce3e
serhiimelnyk <smrlyman@gmail.com>
2026-02-14 12:17:28 +0200

    fix: update accuracy calculation

--- src/components/pages/Main/Stats/Stats.tsx
+++ src/components/pages/Main/Stats/Stats.tsx
@@ -13,7 +13,7 @@ const Stats = () => {
     <section className="h-full flex items-center justify-center">
       <div>
         <span className="text-3xl inline-block">
-          WPM: {wpm} | Accuracy: {(accuracy * 100).toFixed(1)}%
+          WPM: {wpm} | Accuracy: {accuracy.toFixed(1)}%
         </span>
       </div>
     </section>
