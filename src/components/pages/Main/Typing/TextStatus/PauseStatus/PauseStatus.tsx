8691d3f4bfab655111100868a97e04edd7e8cbf5
serhiimelnyk <smrlyman@gmail.com>
2026-02-15 12:59:47 +0200

    feat: update Pause

--- src/components/pages/Main/Typing/TextStatus/PauseStatus/PauseStatus.tsx
+++ src/components/pages/Main/Typing/TextStatus/PauseStatus/PauseStatus.tsx
@@ -4,17 +4,17 @@ const PauseOverlay = () => {
   const isPaused = usePause((state) => state.values.isPaused);
 
   return (
-    <div>
-      Pause{" "}
+    <>
+      <span>Pause</span>
       <kbd
         className={`inline-block ${
-          isPaused ? "bg-green-600" : "bg-red-600"
-        } px-2 py-1 w-12 text-center font-bold text-gray-50 rounded-sm
+          isPaused ? "bg-green-600" : "bg-neutral-600"
+        } px-2 py-0.5 text-sm text-gray-50 rounded-xs
           transition-colors duration-200`}
       >
-        Esc
+        <span>Esc</span>
       </kbd>
-    </div>
+    </>
   );
 };
 
