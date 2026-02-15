38260743b0e19ef96bab2435a5412b55a3e21346
serhiimelnyk <smrlyman@gmail.com>
2026-02-15 12:59:02 +0200

    feat: update ModeSelector

--- src/components/pages/Main/Typing/TextStatus/ModeSelector/ModeSelector.tsx
+++ src/components/pages/Main/Typing/TextStatus/ModeSelector/ModeSelector.tsx
@@ -1,9 +1,18 @@
 import { TypingModes, type TypingMode } from "@/types";
-import Button from "@/components/common/Button/Button";
+import {
+  Infinity as InfinityIcon,
+  Timer as TimerIcon,
+  type LucideIcon,
+} from "lucide-react";
 
 interface Props {
   setMode: React.Dispatch<React.SetStateAction<TypingMode>>;
 }
+
+const modeIcons: Record<TypingMode, LucideIcon> = {
+  infinite: InfinityIcon,
+  timer: TimerIcon,
+};
 
 const ModeSelector = ({ setMode }: Props) => {
   const handleModeChange = (mode: TypingMode) => {
@@ -11,13 +20,28 @@ const ModeSelector = ({ setMode }: Props) => {
   };
 
   return (
-    <>
-      {TypingModes.map((mode) => (
-        <Button key={mode} onClick={() => handleModeChange(mode)}>
-          {mode}
-        </Button>
-      ))}
-    </>
+    <div className="flex border border-neutral-500 rounded-sm">
+      {TypingModes.map((mode) => {
+        const Icon = modeIcons[mode];
+        return (
+          <>
+            <button
+              className="flex group items-center gap-1 px-2 py-1"
+              key={mode}
+              onClick={() => handleModeChange(mode)}
+            >
+              <Icon
+                size={20}
+                className="group-hover:text-neutral-500 transition-colors duration-200"
+              />
+              <span className="group-hover:text-green-600 transition-colors duration-200">
+                {mode}
+              </span>
+            </button>
+          </>
+        );
+      })}
+    </div>
   );
 };
 
